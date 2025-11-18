020a9abf9ba79a667726a993a32e20c51949bebb
Haris <harisafaq22@gmail.com>
2025-11-18 12:29:21 +0500

    "Primary+ Secondry + Constructor with init Block "

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,20 @@ fun main() {
     student.rollNumber = 17
     student.displayInfo()
 
+
+    // oBJECT OF the class with constructor
+
+    val student1: Student1= Student1(
+        firstName="Arish",
+        lastName="Khattak",
+    )
+    student1.displayInfo()
+
+    // Object of Constructor with init block
+
+    val bike : Bike =Bike(model = "ninja h2r")
+
+
 }
 
 
@@ -24,5 +38,36 @@ class Student{
 
     fun displayInfo(){
         println("First Name: $firstName, Last Name: $lastName, Age: $age, Gender: $gender, Roll Number: $rollNumber")
+    }
+}
+
+
+// Class and Object with Constructor
+
+class Student1{
+    var firstName = ""
+    var lastName = ""
+    constructor(firstName: String, lastName: String,){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    fun  displayInfo(){
+        println("First Name: $firstName, Last Name: $lastName")
+    }
+}
+
+//init Block with Constructor, but eske lie hamme primary construtor ko use karna huta ju above use kia hai woh secondary contructor hai
+
+
+class Bike(var model: String){
+
+    // declare any global variable here
+
+    var formattedBikeModel=""
+
+    init{
+
+        formattedBikeModel= model.uppercase()
+        println(formattedBikeModel)
     }
 }
